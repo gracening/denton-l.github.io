1db97ed8141b722e8747aa75b8b0ec12c30d43e3
Denton-L <liu.denton+github@gmail.com>
2016-01-19 06:23:08 -0500

    Allowed direct navigation to a certain project using a query string

--- js/projects.js
+++ js/projects.js
@@ -1,13 +1,39 @@
-var projectNumber = 0;
+var projectNumber;
 var projects;
 
 $.getJSON("/resources/projectlist.json", function(json) {
+		var name = getQueryValue("project");
 		projects = json;
+
+		if (name !== null) {
+			for (projectNumber = projects.length - 1; projectNumber > 0; projectNumber--) {
+				if (projects[projectNumber].query === name) {
+					break;
+				}
+			}
+		} else {
+			projectNumber = 0;
+		}
+
 		loadProject(projectNumber);
+
 		for (var i = 0; i < projects.length; i++) {
 				new Image().src = projects[i].image;
 		}
 });
+
+function getQueryValue(key) {
+	var pairs = window.location.search.substring(1).split("&");
+	for (var i = 0; i < pairs.length; i++) {
+		var pair = pairs[i].split("=");
+		if (pair.length != 2) {
+			continue;
+		} else if (pair[0] === key) {
+			return pair[1];
+		}
+	}
+	return null;
+}
 
 function loadProject(number) {
 		var project = projects[number];
