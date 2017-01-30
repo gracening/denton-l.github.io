aebb82cf969bb5f8e5b522860600e13488e52b6d
Denton Liu <liu.denton@gmail.com>
2017-01-29 16:27:38 -0800

    Make projects use mustache templates

--- js/projects.js
+++ js/projects.js
@@ -1,55 +1,33 @@
-var projectNumber;
-var projects;
+var _projects;
+var _projectNumber;
 
-$.getJSON("/resources/projectlist.json", function(json) {
-		var name = getQueryValue("project");
-		projects = json;
-
-		if (name !== null) {
-			for (projectNumber = projects.length - 1; projectNumber > 0; projectNumber--) {
-				if (projects[projectNumber].query === name) {
-					break;
-				}
-			}
-		} else {
-			projectNumber = 0;
-		}
-
-		loadProject(projectNumber);
-
-		for (var i = 0; i < projects.length; i++) {
-				new Image().src = projects[i].image;
-		}
-});
-
-function getQueryValue(key) {
-	var pairs = window.location.search.substring(1).split("&");
-	for (var i = 0; i < pairs.length; i++) {
-		var pair = pairs[i].split("=");
-		if (pair.length != 2) {
-			continue;
-		} else if (pair[0] === key) {
-			return pair[1];
-		}
+function loadProject(next, previous) {
+	_projects[next].style.display = "inherit";
+	if (previous !== undefined) {
+		_projects[previous].style.display = "none";
 	}
-	return null;
+	animateTyping([_projects[next].querySelector(".projecttitle")]);
 }
 
-function loadProject(number) {
-		var project = projects[number];
-		clearInterval(_interval);
-		$("#imagesection").css("background-image", "url(" +project.image +")");
-		$("#projecttitle").text(project.title);
-		$("#projectdescription").html(project.description);
-		animateTyping($(".typetarget"));
+function changeProject(delta) {
+	var nextProject = (_projectNumber + delta + _projects.length) % _projects.length;
+	loadProject(nextProject, _projectNumber);
+	_projectNumber = nextProject;
 }
 
-function nextProject() {
-		projectNumber = ++projectNumber % projects.length;
-		loadProject(projectNumber);
-}
+window.addEventListener("load", function () {
+	var buttons = document.querySelectorAll(".projectbutton");
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].style.display = "inherit";
+	}
 
-function previousProject() {
-		projectNumber = --projectNumber < 0 ? projects.length - 1 : projectNumber;
-		loadProject(projectNumber);
-}
+	_projects = document.querySelectorAll(".project");
+	_projectNumber = window.location.hash == "" ?
+		0 : Math.max(Array.from(_projects) .indexOf(document.querySelector(window.location.hash)), 0);
+
+	for (var i = 0; i < _projects.length; i++) {
+		_projects[i].style.display = "none";
+	}
+
+	loadProject(_projectNumber);
+});
--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 var _interval = 0;
 
-window.onload = function() {
-	_notStopped = true;
-	animateTyping(document.getElementsByClassName("typetarget"));
-}
-
 function animateTyping(objects) {
 	if (objects.length <= 0) {
 		return;
@@ -12,8 +7,8 @@ function animateTyping(objects) {
 	var typeobjects = [];
 	for (var i = 0; i < objects.length; i++) {
 		typeobjects[i] = {
-				object: objects[i],
-				text: objects[i].innerText
+			object: objects[i],
+			text: objects[i].innerText
 		};
 		objects[i].innerText = "";
 	}
@@ -22,16 +17,20 @@ function animateTyping(objects) {
 
 function type(typeobjects) {
 	var i = 0;
-	var interval = _interval = setInterval(function() {
-			typeobjects[0].object.innerText = typeobjects[0].text.substring(0, ++i);
-			typeobjects[0].object.innerHTML += "<span id=\"cursor\"></span>";
-			if (i >= typeobjects[0].text.length) {
-				clearInterval(interval);
-				if (typeobjects.length > 1) {
-					typeobjects[0].object.innerText = typeobjects[0].text;
-					typeobjects.splice(0, 1)
-					type(typeobjects);
-				}
+	var interval = _interval = setInterval(function () {
+		typeobjects[0].object.innerText = typeobjects[0].text.substring(0, ++i);
+		typeobjects[0].object.innerHTML += "<span id=\"cursor\"></span>";
+		if (i >= typeobjects[0].text.length) {
+			clearInterval(interval);
+			if (typeobjects.length > 1) {
+				typeobjects[0].object.innerText = typeobjects[0].text;
+				typeobjects.splice(0, 1)
+				type(typeobjects);
 			}
-		}, 15);
+		}
+	}, 15);
 }
+
+window.addEventListener("load", function () {
+	animateTyping(document.querySelectorAll(".typetarget"));
+});
